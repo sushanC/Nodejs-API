7fd21fefda32068ebc58750246b617fb85d02a85
Sushan Achar <sushanachar2006@gmail.com>
2025-06-13 22:27:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,9 @@ app.use('/products', require('./controllers/products'))
 app.use('/users', require('./controllers/users'))
 app.use('/about', require('./controllers/About'))
 
-
-
-
 app.get('/',(req,res)=>{
     res.json({message:"Hello from nodeAPI"})
 });
-
-
-
-
 app.listen(5000,()=>{
     console.log("server started on port 5000"); 
 });

701460cf61ea839314a8753af0b5dce868d86683
Sushan Achar <sushanachar2006@gmail.com>
2025-06-13 22:26:31 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.json({message:"Displaying user"})
+    res.json({message:"Displaying user Detailas"})
 });
 router.get('/:userID',(req,res)=>{
     console.log(req.params.userID);
     const userID=req.params.userID
     res.json({message:`Displaying the user ${userID}`})
 })
-
-
-
-
 module.exports=router;
